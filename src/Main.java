d36645db110e21e0509445fdca913b58bf65415f
Grisha <guruwolf8@gmail.com>
2023-05-29 17:06:28 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,18 @@ public class Main {
     }
     public static void task2(){
         System.out.println("Задача 2.");
+       int clientOs=1;
+       int clientDeviceYear=2015;
+       if (clientOs==0&&clientDeviceYear>=2015) {
+           System.out.println("Установите версию приложения для iOS по ссылке.");
+       }else if (clientOs == 0 && clientDeviceYear < 2015){
+               System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+           }
+       if (clientOs==1&&clientDeviceYear>=2015)
+           System.out.println("Установите версию приложения для Android по ссылке.");
+           else if (clientOs==1&&clientDeviceYear<2015)
+           System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        //Проверенно всеми 4 вариантами
     }
     public static void task3(){
         System.out.println("Задача 3.");
